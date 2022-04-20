23a7d924e5a7a1c3d9577744cbf47b1c0b5397c5
samsamsam34 <103471151+samsamsam34@users.noreply.github.com>
2022-04-20 10:24:28 -0600

    Update vapPressure.py

--- pythonEB_lab/vapPressure.py
+++ pythonEB_lab/vapPressure.py
@@ -8,23 +8,17 @@ def vapPressure(df): #Takes pandas dataframe as argument
 
   esat = np.empty(len(inMet.index)) #Create empty numpy array
 
-  for i in range(len(inMet.index): #Calculate vapor pressure in mb
-    esat[i] = round(6.11*(math.exp((17.3*inMet.iloc[i]['T_C'])/(inMet.iloc[i]['T_C']+237.3))),2)
+  for i in range(len(inMet.index)):  #Calculate vapor pressure in mb
+    esat[i] = round(6.11*(math.exp((17.3*inMet.iloc[i]['T_C'])/(inMet.iloc[i]['T_C']+237.3))),2) 
     #this formula is from Dingman, Appendix D, formula D-7. The output is in mb
   
   inMet['esat'] = esat.tolist() #Add to array
   
   #Convert the saturation vapor pressure to vapor pressure using relative humidity
-  
+
   inMet['ea'] = inMet['esat']*inMet[' RH']
-  
+
   #Take the difference between the ea and esat for use in the latent heat calculations 
-  inMet['vpdiff'] = inMet['ea']-inMet['esat'] #Save to newcolum in df
+  inMet['vpdiff'] = inMet['ea']-inMet['esat']
 
   return inMet #Return updated dataframe
-
-
-
-
-
-
