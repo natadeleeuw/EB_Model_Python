50038314b8a51797bb4fd569b9bf7bf723278e17
natadeleeuw <90713213+natadeleeuw@users.noreply.github.com>
2022-04-26 10:40:35 -0600

    Update sensible_heat.py

--- pythonEB_lab/sensible_heat.py
+++ pythonEB_lab/sensible_heat.py
@@ -16,6 +16,9 @@ Cp = 1005
 Ts = 0
 Ch = 0.003
 
+url = 'https://github.com/samsamsam34/EB_Models_mat/blob/758d9443d7aff14df6aa5a1db57387078188b135/matlabEB_lab/CENMET_INPUT_data.csv?raw=true'
+inMet = pd.read_csv(url,index_col=0)
+
 def sensibleheat(df):
   inMet = df
 
